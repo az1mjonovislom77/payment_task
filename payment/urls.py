10996578b99b29defb77fe5c41050c384c16d8f1
az1mjonovislom77 <azimjonovislomjon77@gmail.com>
2026-04-23 21:40:34 +0500

    done

--- payment/urls.py
+++ payment/urls.py
@@ -6,6 +6,7 @@ urlpatterns = [
     path('item/<int:id>/', item_page),
     path('buy/<int:id>/', buy_item),
     path('buy-order/<int:id>/', buy_order),
+    path('order/<int:id>/', order_page),
     path('buy-intent/<int:id>/', buy_intent),
     path('success/', success_page),
     path('cancel/', cancel_page)
