275a33129f7ab1b4fcf8f198527d3dfbb6912fea
az1mjonovislom77 <azimjonovislomjon77@gmail.com>
2026-04-23 21:34:16 +0500

    done

--- payment/views.py
+++ payment/views.py
@@ -1,3 +1,4 @@
+import stripe
 from django.conf import settings
 from django.http import JsonResponse, HttpResponse
 from django.shortcuts import get_object_or_404, render
@@ -7,8 +8,12 @@ from .services.stripe import create_checkout_session, create_order_session, crea
 
 def buy_item(request, id):
     item = get_object_or_404(Item, id=id)
-    session = create_checkout_session(item)
-    return JsonResponse({'id': session.id})
+
+    try:
+        session = create_checkout_session(item)
+        return JsonResponse({'id': session.id})
+    except stripe.error.StripeError as e:
+        return JsonResponse({'error': str(e)}, status=400)
 
 
 def item_page(request, id):
@@ -21,8 +26,12 @@ def item_page(request, id):
 
 def buy_order(request, id):
     order = get_object_or_404(Order, id=id)
-    session = create_order_session(order)
-    return JsonResponse({'id': session.id})
+
+    try:
+        session = create_order_session(order)
+        return JsonResponse({'id': session.id})
+    except stripe.error.StripeError as e:
+        return JsonResponse({'error': str(e)}, status=400)
 
 
 def buy_intent(request, id):
@@ -31,12 +40,22 @@ def buy_intent(request, id):
     return JsonResponse({'client_secret': intent.client_secret})
 
 
+def order_page(request, id):
+    order = get_object_or_404(Order, id=id)
+
+    return render(request, "order.html", {
+        "order": order,
+        "STRIPE_PUBLISHABLE_KEY": settings.STRIPE_PUBLISHABLE_KEY
+    })
+
+
 def success_page(request):
     return HttpResponse("Success")
 
 
 def cancel_page(request):
     return HttpResponse("Cancel")
+
 
 def home_page(request):
     return HttpResponse("Welcome")
